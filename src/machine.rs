b81f6eeede03768dc1f95f340d5e93f74f9abf88
David L. Inman <davidlevere@gmail.com>
2023-05-05 23:58:48 -0400

    machine word handling cases

--- src/machine.rs
+++ src/machine.rs
@@ -12,8 +12,26 @@ pub fn execute( prog : &str, env : &mut Env ) -> Result<(), MachineError> {
 
     'main_loop : loop {
         match &*current_word {
-            Word::Func(_) => {
+            Word::Func(words) if ip == words.len() => {
+                // End of word
+                env.pop_def_level();
+                match env.pop_func_restore_point() {
+                    Some((word, new_ip)) => { 
+                        current_word = word;
+                        ip = new_ip;
+                    },
+                    None => { break 'main_loop; },
+                }
+            },
+            Word::Func(words) if ip > words.len() => {
+                todo!(); // error condition
+            },
+            Word::Func(words) => {
+                env.push_func_restore_point(&current_word, ip + 1);
+                env.new_def_level();
 
+                current_word = words[ip].clone();
+                ip = 0;
             },
             Word::Il(instrs) => {
                 for instr in instrs.iter() {
