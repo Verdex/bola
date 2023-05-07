010c242d0c38f34220f022c744dc8092199d3e43
David L. Inman <davidlevere@gmail.com>
2023-05-06 22:31:54 -0400

    def word definition

--- src/standard/word.rs
+++ src/standard/word.rs
@@ -26,3 +26,31 @@ pub fn push_symbol(value : String) -> Rc<Word> {
                                 };
     Rc::new(Word::Il(vec![r]))
 }
+
+pub fn def_word() -> Rc<Word> {
+    fn word(env : &mut Env) -> Result<(), MachineError> {
+        let sym = env.pop_data().unwrap(); // TODO error
+        let def = env.pop_data().unwrap(); // TODO error
+
+        if let (IlData::Symbol(name), IlData::List(code)) = (sym, def) {
+            let mut func_addrs = vec![];
+            for func_name in code.iter() {
+                match func_name {
+                    IlData::Symbol(word_name) => {
+                        let addr = env.lookup_word(word_name).unwrap(); // TODO
+                        func_addrs.push(addr);
+                    },
+                    _ => todo!(), // TODO unreachable?
+                }
+            }
+            env.define_word(name, Word::Func(func_addrs))?;
+        }
+        else {
+            // TODO
+        }
+
+        Ok(())
+    }
+
+    Rc::new(Word::Il(vec![Il::Instr { name: "def_word".to_owned(), f : word }]))
+}
