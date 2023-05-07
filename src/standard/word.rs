1832153823d38884dd9d6809d3563684c27df9af
David L. Inman <davidlevere@gmail.com>
2023-05-06 22:40:53 -0400

    standard words will be put into the Rc when they're defined with the Env method

--- src/standard/word.rs
+++ src/standard/word.rs
@@ -1,33 +1,31 @@
 
-use std::rc::Rc;
-
 use crate::data::*;
 
-pub fn push_float(value : f64) -> Rc<Word> {
+pub fn push_float(value : f64) -> Word {
     let r = Il::InstrWithFloat { name : "push_float".to_owned()
                                , param: value
                                , f: |param, env| { env.push_data(IlData::Float(param)); Ok(()) }
                                };
-    Rc::new(Word::Il(vec![r]))
+    Word::Il(vec![r])
 }
 
-pub fn push_string(value : String) -> Rc<Word> {
+pub fn push_string(value : String) -> Word {
     let r = Il::InstrWithString { name : "push_string".to_owned()
                                 , param: value
                                 , f: |param, env| { env.push_data(IlData::String(param)); Ok(()) }
                                 };
-    Rc::new(Word::Il(vec![r]))
+    Word::Il(vec![r])
 }
 
-pub fn push_symbol(value : String) -> Rc<Word> {
+pub fn push_symbol(value : String) -> Word {
     let r = Il::InstrWithSymbol { name : "push_symbol".to_owned()
                                 , param: value
                                 , f: |param, env| { env.push_data(IlData::Symbol(param)); Ok(()) }
                                 };
-    Rc::new(Word::Il(vec![r]))
+    Word::Il(vec![r])
 }
 
-pub fn def_word() -> Rc<Word> {
+pub fn def_word() -> Word {
     fn word(env : &mut Env) -> Result<(), MachineError> {
         let sym = env.pop_data().unwrap(); // TODO error
         let def = env.pop_data().unwrap(); // TODO error
@@ -52,5 +50,5 @@ pub fn def_word() -> Rc<Word> {
         Ok(())
     }
 
-    Rc::new(Word::Il(vec![Il::Instr { name: "def_word".to_owned(), f : word }]))
+    Word::Il(vec![Il::Instr { name: "def_word".to_owned(), f : word }])
 }
