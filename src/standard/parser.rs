bf1f7e490ff709fb82009b065ff4696ef26fa942
David L. Inman <davidlevere@gmail.com>
2023-05-07 18:13:04 -0400

    parse word symbols

--- src/standard/parser.rs
+++ src/standard/parser.rs
@@ -29,3 +29,46 @@ pub fn parse_whitespace() -> Word {
 
     Word::Il(vec![Il::Instr { name: "parse_whitespace".to_owned(), f : word }])
 }
+
+pub fn parse_word_symbol() -> Word {
+    fn word(env : &mut Env) -> Result<(), MachineError> {
+        let start_index = env.pop_data_as("parse_word_symbol::start_index".to_owned(), pattern!(IlData::Usize(x) => x))?;
+        let input = env.pop_data_as("parse_word_symbol::start_index".to_owned(), pattern!(IlData::String(x) => x))?;
+
+        let mut target = input[start_index..].chars();
+
+        let mut i : usize = 0;
+        let mut word_symbol = vec![];
+
+        match target.next() {
+            Some(c) if c.is_alphabetic() || c == '_' => { i += 1; word_symbol.push(c); },
+            _ => { 
+                env.push_data(IlData::String(input));
+                env.push_data(IlData::Usize(start_index));
+                env.push_data(IlData::Symbol(ERROR_SYM.to_owned()));
+                return Ok(());
+            },
+        }
+
+        loop {
+            match target.next() {
+                Some(c) if c.is_alphanumeric() || c == '_' => { i += 1; word_symbol.push(c); },
+                _ => { break; },
+            }
+        }
+
+        let word_symbol = word_symbol.into_iter().collect::<String>();
+
+        let target = env.lookup_word(&word_symbol)?;
+        env.push_data(IlData::Word(target));
+
+        env.push_data(IlData::String(input));
+        env.push_data(IlData::Usize(start_index + i));
+        env.push_data(IlData::Symbol(RESULT_SYM.to_owned()));
+
+
+        Ok(())
+    }
+
+    Word::Il(vec![Il::Instr { name: "parse_word_symbol".to_owned(), f : word }])
+}
