42bf13192737973c33deaf29a8dfa118cfd45067
David L. Inman <davidlevere@gmail.com>
2023-05-10 22:34:59 -0400

    inc prog

--- src/standard/parser/def_word_parser.rs
+++ src/standard/parser/def_word_parser.rs
@@ -1,4 +1,6 @@
 
+use crate::data::*;
+
 pub fn parse_def_word() -> Word {
     fn word(env : &mut Env) -> Result<(), MachineError> {
         let start_index = env.pop_data_as("parse_word_symbol::start_index".to_owned(), pattern!(IlData::Usize(x) => x))?;
