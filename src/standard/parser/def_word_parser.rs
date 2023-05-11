1239a62db10079cfb27304ad42260074584f4d26
David L. Inman <davidlevere@gmail.com>
2023-05-10 22:34:24 -0400

    def word parser stub

---
+++ src/standard/parser/def_word_parser.rs
@@ -0,0 +1,9 @@
+
+pub fn parse_def_word() -> Word {
+    fn word(env : &mut Env) -> Result<(), MachineError> {
+        let start_index = env.pop_data_as("parse_word_symbol::start_index".to_owned(), pattern!(IlData::Usize(x) => x))?;
+        let input = env.pop_data_as("parse_word_symbol::start_index".to_owned(), pattern!(IlData::String(x) => x))?;
+        Ok(())
+    }
+    Word::Il(vec![])
+}
--- src/standard/parser/mod.rs
+++ src/standard/parser/mod.rs
@@ -1,6 +1,8 @@
 
 mod parser;
 mod parser_parser;
+mod def_word_parser;
 
+pub use def_word_parser::*;
 pub use parser_parser::*;
 pub use parser::*;
